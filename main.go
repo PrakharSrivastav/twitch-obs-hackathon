71f6a387b252dcc9bffcc9aecd7f525de560b439
PrakharSrivastav <srivprakhar@gmail.com>
2022-05-10 11:06:12 +0200

    streaming working

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	servicebus "github.com/Azure/azure-service-bus-go"
 	"github.com/PrakharSrivastav/twitch-obs-hackathon/azure"
 	"github.com/PrakharSrivastav/twitch-obs-hackathon/obs"
-	"github.com/andreykaipov/goobs/api/requests/scenes"
 	"log"
 )
 
@@ -17,19 +16,15 @@ func main() {
 	if err != nil {
 		log.Fatalln("azure connection error : ", err)
 	}
-	defer closeConnections(err, sbClient)()
-
 	obsClient, err := obs.NewClient()
 	if err != nil {
 		log.Fatalln("OBS obsClient error : ", err)
 	}
-
-	mp := MessagePrinter{cc: obsClient}
+	defer closeConnections(sbClient, obsClient)
 
 	// some logic here
-
 	for {
-		err = sbClient.Queue.ReceiveOne(context.Background(), mp)
+		err = sbClient.Queue.ReceiveOne(context.Background(), MessagePrinter{cc: obsClient})
 		if err != nil {
 			log.Fatalln("receive error : ", err)
 		}
@@ -39,14 +34,19 @@ func main() {
 
 }
 
-func closeConnections(err error, client *azure.SBClient) func() {
-	return func() {
-		err = client.Close()
-		if err != nil {
-			log.Println(err)
-		}
-		log.Println("connection closed")
+func closeConnections(client *azure.SBClient, cc *obs.OBSClient) {
+
+	err := client.Close()
+	if err != nil {
+		log.Println(err)
 	}
+
+	err = cc.Client.Conn.Close()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("connection closed")
+
 }
 
 type MessagePrinter struct {
@@ -54,14 +54,51 @@ type MessagePrinter struct {
 }
 
 func (mp MessagePrinter) Handle(ctx context.Context, msg *servicebus.Message) error {
-	fmt.Println(string(msg.Data))
-	
-	scene := string(msg.Data)
 
-	_, err := mp.cc.Client.Scenes.SetCurrentScene(&scenes.SetCurrentSceneParams{SceneName: scene})
+	var mm SbMessage
+	err := json.Unmarshal(msg.Data, &mm)
 	if err != nil {
-		log.Println("scene change error :", err)
+		log.Printf("unknown message %s \n", string(msg.Data))
+	}
+
+	log.Printf("message %v \n", mm)
+
+	switch mm.Action {
+	case "SwitchScene":
+		scene := mm.Options["scene"]
+		if scene != "" {
+			if err := mp.cc.SwitchScene(scene); err != nil {
+				log.Printf("error : %v \n", err)
+			}
+		}
+
+	case "StartStream":
+		if err := mp.cc.StartStream(); err != nil {
+			log.Printf("error : %v \n", err)
+		}
+
+	case "StopStream":
+		if err := mp.cc.StopStream(); err != nil {
+			log.Printf("error : %v \n", err)
+		}
+
+	case "QuestionAnswered":
+		// aggregate
+		// calculate if highest count is correct
+		// if correct
+		// switch to next scene
+		// go back to start
+
+	default:
+		log.Println("unknown action ", mm.Action)
 	}
 
 	return msg.Complete(ctx)
 }
+
+type SbMessage struct {
+	Action  string            `json:"action"`
+	Options map[string]string `json:"options"`
+}
+
+var currentScene = "BRB"
--- obs/obs.go
+++ obs/obs.go
@@ -1,7 +1,11 @@
 package obs
 
 import (
+	"fmt"
 	"github.com/andreykaipov/goobs"
+	"github.com/andreykaipov/goobs/api/events"
+	"github.com/andreykaipov/goobs/api/requests/scenes"
+	"github.com/andreykaipov/goobs/api/requests/streaming"
 	"log"
 	"os"
 )
@@ -21,6 +25,56 @@ func NewClient() (*OBSClient, error) {
 		log.Println("connection error")
 		return nil, err
 	}
+	go func() {
+		for event := range client.IncomingEvents {
+			switch e := event.(type) {
+			case *events.SourceVolumeChanged:
+				fmt.Printf("Volume changed for %-25q: %f\n", e.SourceName, e.Volume)
+			case *events.StreamStarted:
+				fmt.Println("Stream started successfully")
+			case *events.StreamStopped:
+				fmt.Println("Stream stopped successfully")
+			case *events.ScenesChanged:
+				fmt.Println("Scene changed successfully")
+
+			default:
+				log.Printf("Unhandled event: %#v", e.GetUpdateType())
+			}
+		}
+	}()
 
 	return &OBSClient{Client: client}, nil
+}
+
+func (o *OBSClient) SwitchScene(sceneName string) error {
+	resp, err := o.Client.Scenes.SetCurrentScene(&scenes.SetCurrentSceneParams{SceneName: sceneName})
+	if err != nil {
+		log.Println("scene change error :", err)
+		return err
+	}
+
+	log.Printf("%v \n", resp)
+	return nil
+}
+
+func (o *OBSClient) StartStream() error {
+	resp, err := o.Client.Streaming.StartStreaming(&streaming.StartStreamingParams{})
+	if err != nil {
+		log.Println("start stream error :", err)
+		return err
+	}
+
+	log.Printf("%v \n", resp)
+	return nil
+}
+
+func (o *OBSClient) StopStream() error {
+	resp, err := o.Client.Streaming.StopStreaming(&streaming.StopStreamingParams{})
+	if err != nil {
+		log.Println("stop stream error :", err)
+		return err
+	}
+
+	log.Printf("%v \n", resp)
+	return nil
 }
